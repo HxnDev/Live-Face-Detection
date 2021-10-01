ea1d6905b2b725afc7301e5d9489db0813187dcb
Hxn <chhxnshah@gmail.com>
2021-10-01 09:07:06 +0500

    Uploading Files

---
+++ main.py
@@ -0,0 +1,26 @@
+import cv2
+import mediapipe as mp
+
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0)
+with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame")
+            continue
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        results = face_detection.process(image)
+
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.detections:
+            for detection in results.detections:
+                mp_drawing.draw_detection(image, detection)
+        cv2.imshow('Face Detection', image)
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+cap.release()
